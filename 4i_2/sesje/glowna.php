9f4f68e72acba5a546429e3288bed43f9c723b7a
laptopik208 <laptoppp208@gmail.com>
2025-01-14 11:44:33 +0100

    works 2 added comments

--- 4i_2/sesje/glowna.php
+++ 4i_2/sesje/glowna.php
@@ -6,6 +6,18 @@
     <title>Main</title>
 </head>
 <body>
-    
+    <?php
+        session_start(); // Rozpocznij sesję
+        //$_SESSION['logowanie'] = $imie; 
+        //ucfirst($imie); // Zmienia pierwszą literę na dużą
+        echo 'Witaj,'.ucfirst($_SESSION['logowanie']) .'!<br>'; 
+
+        echo 'Jesteś zalogowany/a!<br>'; // Wyświetla komunikat o zalogowaniu
+
+        echo '<a href="wyloguj.php">Wyloguj</a>'; // Wyświetla link do wylogowania
+
+
+        //stwórz tablce uzytkowników z loginami i hasłami kod php ma wyszykiwac login i hasło w tej tablicy
+    ?>
 </body>
 </html>
--- 4i_2/sesje/logowanie.php
+++ 4i_2/sesje/logowanie.php
@@ -18,6 +18,16 @@
     </form>
     
     <?php
+        $baza = array(
+            'anna' => 'ania123',
+            'katarzyna123' => 'kasia123',
+            'Jan' => 'kowalski',
+            'adam' => 'Luncia15',
+            'Marek' => 'Jarek'
+
+        );///tablica z loginami i hasłami
+
+
         session_start(); // Rozpocznij sesję
 
         // Sprawdza czy użytkownik jest już zalogowany
@@ -28,15 +38,51 @@
         // Sprawdza, czy formularz logowania został przesłany
         elseif(isset($_POST['nazwa']) && isset($_POST['haslo'])){
             // Zweryfikuj dane logowania
-            if($_POST['nazwa'] == 'anna' && $_POST['haslo'] == 'ania123'){
-                $_SESSION['logowanie'] = $_POST['nazwa']; // Ustawia zmienną sesji
-                header('Location: glowna.php'); // Przekierowywuje na stronę główną
-                exit();
+            foreach ($baza as $user => $haslo) { //porównuje co zostało wpisanie przez użytkownika i czy są te dane w bazie danych 
+                if($_POST['nazwa'] == $user && $_POST['haslo'] == $haslo){
+                    $_SESSION['logowanie'] = $_POST['nazwa']; // Ustawia zmienną sesji
+                    header('Location: glowna.php'); // Przekierowywuje na stronę główną
+                    exit();
+                }
+                else{
+                   // echo 'Błędne dane logowania.<br>'; // Wyświetla komunikat o błędzie
+                }
             }
-            else{
-                echo 'Błędne dane logowania'; // Wyświetla komunikat o błędzie
-            }
+            echo 'Błędne dane logowania.<br>'; // Wyświetla komunikat o błędzie
+            
         }
+
+                    /*
+            Tworzenie, zarządzanie i dodawanie ciasteczek oraz sesji:
+
+            1. Tworzenie ciasteczka:
+            setcookie('nazwa_ciasteczka', 'wartość', time() + 3600); // Ciasteczko ważne przez 1 godzinę
+
+            2. Odczyt ciasteczka:
+            if(isset($_COOKIE['nazwa_ciasteczka'])) {
+                echo $_COOKIE['nazwa_ciasteczka'];
+            }
+
+            3. Usunięcie ciasteczka:
+            setcookie('nazwa_ciasteczka', '', time() - 3600); // Ustawienie czasu wygaśnięcia w przeszłości
+
+            4. Rozpoczęcie sesji:
+            session_start(); // Musi być wywołane na początku skryptu przed jakimkolwiek wyjściem HTML
+
+            5. Ustawienie zmiennej sesji:
+            $_SESSION['nazwa_zmiennej'] = 'wartość';
+
+            6. Odczyt zmiennej sesji:
+            if(isset($_SESSION['nazwa_zmiennej'])) {
+                echo $_SESSION['nazwa_zmiennej'];
+            }
+
+            7. Usunięcie zmiennej sesji:
+            unset($_SESSION['nazwa_zmiennej']);
+
+            8. Zakończenie sesji:
+            session_destroy(); // Usuwa wszystkie dane sesji
+            */
     ?>
 </body>
 </html>
